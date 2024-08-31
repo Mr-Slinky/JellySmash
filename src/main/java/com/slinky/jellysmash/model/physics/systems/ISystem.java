158cf8b9e7ee69d2262765e53f3a45e19495ad1d
Kheagen Haskins <slinky.haskins@gmail.com>
2024-08-31 16:08:54 +0200

    refactor: Renamed System interface to ISystem to avoid naming conflicts with Java's System class

--- src/main/java/com/slinky/jellysmash/model/physics/systems/ISystem.java
+++ src/main/java/com/slinky/jellysmash/model/physics/systems/ISystem.java
@@ -1,19 +1,19 @@
 package com.slinky.jellysmash.model.physics.systems;
 
 /**
- * The {@code System} interface serves as a marker interface for all systems
+ * The {@code ISystem} interface serves as a marker interface for all systems
  * within the Entity Component System (ECS) architecture.
  *
  * <p>
  * In the context of an ECS, a system is responsible for processing entities
- * that possess a specific set of components. The {@code System} interface
+ * that possess a specific set of components. The {@code ISystem} interface
  * provides a common type for all systems, allowing them to be managed and
  * operated upon collectively. By implementing this interface, a class is
  * recognised as a system within the ECS framework.
  * </p>
  *
  * <p>
- * As a marker interface, {@code System} does not define any methods. Its
+ * As a marker interface, {@code ISystem} does not define any methods. Its
  * primary purpose is to provide a type that identifies a class as a system in
  * the ECS pattern, facilitating type-safe operations and system management.
  * </p>
@@ -26,4 +26,4 @@ package com.slinky.jellysmash.model.physics.systems;
  * @see    Component
  * @see    Entity
  */
-public interface System {}
+public interface ISystem {}
